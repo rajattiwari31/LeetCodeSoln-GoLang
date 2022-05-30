e05aecf2a6f0a29e5ecae8a6a0d715b6e5929452
rajat <rajattiwari3101@gmail.com>
2022-05-30 22:34:35 +0530

    Update L49.go

--- Medium/L49.go
+++ Medium/L49.go
@@ -1,18 +1,22 @@
 package Medium
 
 import (
-	"sort"
 	"strings"
 )
 
 /*
 Approach -
-	- First sort all the strings
-	- then store the sorted key in the hashmap
-	- now compare all the string with stored sorted string
-
+	Approach 1
+		- First sort all the strings
+		- then store the sorted key in the hashmap
+		- now compare all the string with stored sorted string
+	Approach 2
+		- Instead of sorting the string, we should create a unique ID
+		- Then we will use this UniqueID to check in the hashmap
+		- By this we can reduce the O(nlogn) -> o(n)
 Time Complexity -
 	- O(nlogn) - for soting the key
+	- o(n) 	- In second approach
 
 Space Complexity -
 	- o(n) - for the hashmap
@@ -26,13 +30,18 @@ func groupAnagrams(strs []string) [][]string {
 	res := make([][]string, 0)
 
 	mp := make(map[string][]string)
-
+	/* Approach 1
 	for _, s := range strs {
 		sortString := sortAnagram(s)
 		if _, ok := mp[sortString]; !ok {
 			mp[sortString] = []string{}
 		}
 		mp[sortString] = append(mp[sortString], s)
+	}*/
+
+	for _, s := range strs {
+		strId := getID(s)
+		mp[strId] = append(mp[strId], s)
 	}
 
 	for _, value := range mp {
@@ -41,8 +50,26 @@ func groupAnagrams(strs []string) [][]string {
 	return res
 }
 
+/* Approach - 1
 func sortAnagram(str string) string {
 	sortString := strings.Split(str, "")
 	sort.Strings(sortString)
 	return strings.Join(sortString, "")
+}*/
+
+// Approach 2
+func getID(s string) string {
+	arr := make([]byte, 26)
+
+	for _, k := range s {
+		arr[k-'a']++
+	}
+
+	var sb strings.Builder
+
+	for _, k := range arr {
+		sb.WriteByte(k)
+	}
+
+	return sb.String()
 }
